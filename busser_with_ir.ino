86be51ac14114adffff6d5eb5d8e9a4ee0aa45bc
JISIHNU VIJAYAN C K <48761358+jishnurippon@users.noreply.github.com>
2019-03-20 19:42:02 +0530

    Add files via upload

---
+++ busser_with_ir.ino
@@ -0,0 +1,23 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(A0,INPUT);
+  pinMode(D4,OUTPUT);
+  Serial.begin(115200);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println(analogRead(A0));
+  delay(5);
+  /*if(analogRead(A0)<1000)
+  {
+    digitalWrite(D4,LOW);
+    delay(2000);
+  }
+  else
+  {
+    digitalWrite(D4,HIGH);
+    delay(2000);
+  }*/
+
+}
